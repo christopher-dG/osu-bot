e4b74df9dcf46734b9d57cec34ecdb6a52416506
Chris de Graaf <chrisadegraaf@gmail.com>
2017-04-22 21:43:14 -0500

    Fix Markdown formatting

--- lib/markdown.rb
+++ lib/markdown.rb
@@ -188,7 +188,7 @@ def markdown(post)
   show_player && md += "\n#{player_md}\n"
   md += "***\n\n"
   md += "^(I'm a bot. )[^Source](#{GH_URL})^( | )[^Developer](#{DEV_URL})\n\n"
-  md += "^(Notice a mistake? Read [this](#{GH_URL}/blob/master/error.md."
+  md += "^(Notice a mistake? Read )[^this](#{GH_URL}/blob/master/error.md)^."
 
   log("Generated full comment:\n#{md}")
   return md
