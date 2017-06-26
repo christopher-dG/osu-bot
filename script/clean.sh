c7a90f40a25e36879471f2319323128cc64fe167
Chris de Graaf <chrisadegraaf@gmail.com>
2017-06-26 04:58:53 +0000

    Delete old files

--- script/clean.sh
+++
@@ -1,7 +0,0 @@
-# Keep the rolling log file under 5 MB.
-tail -c 5MB ~/osu!-bot/logs/rolling.log > temp
-mv temp ~/osu!-bot/logs/rolling.log
-
-# Zip all the single-run logs to save space.
-zip ~/osu!-bot/logs/logs.zip $(find ~/osu!-bot/logs -type f -regex ".*[0-9]\.log")
-rm $(find ~/osu!-bot/logs -type f -regex ".*[0-9]\.log")
