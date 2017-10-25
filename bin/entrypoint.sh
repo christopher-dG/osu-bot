0a53be722588fbaf2cf700f58a8533d398312e37
Chris de Graaf <chrisadegraaf@gmail.com>
2017-10-25 20:08:36 +0000

    Add test entrypoint to Docker image

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,6 +4,8 @@ if [[ $1 = 'bash' ]]; then
     exec bash
 elif [[ $1 = 'julia' ]]; then
     exec julia
+elif [[ $1 = 'test' ]]; then
+    exec julia --color=yes -e 'Pkg.test("OsuBot")'
 else
     exec $APP/bin/bot.jl "$@"
 fi
