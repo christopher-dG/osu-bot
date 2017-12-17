cd7c547e5d167852baf276b7bb8b378d92bf7618
Chris de Graaf <chrisadegraaf@gmail.com>
2017-12-17 17:00:47 +0000

    Tweak handler function

--- handler.py
+++ handler.py
@@ -12,9 +12,12 @@ resp = {
     "headers": {},
     "body": json.dumps(body),
 }
+testrun = os.environ.get("LAMBDA_TEST", "").lower() != "false"
 
 
 def handler(event, _):
+    if testrun:
+        print("=== TEST RUN ===")
     p_id = event["queryStringParameters"].get("id", None)
     if p_id is None:
         err = "Missing id parameter"
@@ -34,10 +37,10 @@ def handler(event, _):
     )
     post = praw.models.Submission(rdt, p_id)
     try:
-        if post.saved:
+        if not testrun and post.saved:
             print("Post is already saved")
             return resp
-    except Exception as e:  # Quite likely doesn't exist.n
+    except Exception as e:  # Quite likely doesn't exist.
         print(e)
         resp["statusCode"] = 400
         body["error"] = str(e)
@@ -50,21 +53,21 @@ def handler(event, _):
 
     try:
         result = osubot.main(post.title)
-        if result is not None:
-            ctx, reply = result
+        if result is None:
+            err = "Comment generation failed"
+            print(err)
+            resp["statusCode"] = 500
+            body["error"] = err
+            resp["body"] = json.dumps(body)
+            return resp
+        ctx, reply = result
     except Exception as e:
         print("Exception raised while generating reply: %s" % e)
         resp["statusCode"] = 500
         body["error"] = str(e)
         resp["body"] = json.dumps(e)
         return resp
-    if result is None:
-        err = "Comment generation failed"
-        print(err)
-        resp["statusCode"] = 500
-        body["error"] = err
-        resp["body"] = json.dumps(body)
-        return resp
+
     if not reply:
         err = "Reply is empty"
         print(err)
@@ -73,19 +76,29 @@ def handler(event, _):
         resp["body"] = json.dumps(body)
         return resp
 
-    if any(c.author.name == osubot.consts.reddit_user for c in post.comments):
+    if not testrun and any(
+        c.author.name == osubot.consts.reddit_user if c.author else False
+        for c in post.comments,
+    ):
         print("Post already has a reply")
-        # Don't return here, we'll re-save and upvote just in case.
-    elif os.environ.get("LIVE", "false").lower() == "true":
+        try:
+            post.save()
+            post.upvote()
+        except:
+            pass
+    elif not testrun:
         c = post.reply(reply)
         c.mod.distinguish(sticky=True)
+        post.save()
+        post.upvote()
 
-    post.save()
-    post.upvote()
     body["context"] = ctx.to_dict()
     body["comment"] = reply
     resp["body"] = json.dumps(body)
 
     print("%s\nCommented:\n%s" % (ctx, reply))
+
+    if testrun:
+        print("=== TEST RUN ===")
 
     return resp
